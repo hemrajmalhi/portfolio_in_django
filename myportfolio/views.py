c411c098761e7a2335a7e748cea36022a51afc01
hemraj malhi <hemrajmalhi1234@gmail.com>
2023-01-16 15:16:09 -0800

    some modification

--- myportfolio/views.py
+++ myportfolio/views.py
@@ -3,13 +3,17 @@ from myportfolio.models import Contact
 
 
 # Create your views here.
-
 def home(request):
+    context = {}
     if request.method == 'POST':
         name = request.POST['name']
         email = request.POST['email']
         subject = request.POST['subject']
         message = request.POST['message']
         contact = Contact(name=name, email=email, subject=subject, message=message)
-        contact.save()
-    return render(request, "home.html", {"message": "your message has been successfully sent"})
+        if len(name) < 4 or len(email) < 10 or len(subject) < 5 or len(message) < 6:
+            context = {"error": "Error"}
+        else:
+            contact.save()
+            context = {"message": name}
+    return render(request, "home.html", context)
